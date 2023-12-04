87a2d54cc8e5e93bba447f29ae65ac7c5af28155
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-04 10:54:09 +0200

    rest of backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import bookRoutes from "./routes/bookRoutes.js";
+import cors from "cors";
+import { PORT, mongoDbURL } from "./config.js";
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cors());
+
 app.get("/", (req, res) => {
-  console.log(req);
   return res.status(234).send("Welcome to backend");
 });
 
-app.listen(PORT, () => console.log(`App is listening to port: ${PORT}`));
+app.use("/books", bookRoutes);
+
+mongoose
+  .connect(mongoDbURL)
+  .then(() => {
+    console.log("App connected to the MongoDB database");
+    app.listen(PORT, () => console.log(`App is listening to port: ${PORT}`));
+  })
+  .catch((err) => console.log(err));
